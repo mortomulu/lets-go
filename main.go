d90161c83a184a26d59214d76557ff668a597b3f
mortomulu <mfajrihidayat09@gmail.com>
2024-08-13 23:18:55 +0700

    task 1

--- main.go
+++ main.go
@@ -10,60 +10,60 @@ import (
 
 func main() {
 	fmt.Println("start main")
-	// basics()
+	basics()
 
-	// fmt.Println("sum3Number(1, 2, 3) =", sum3Number(1, 2, 3)) // 6
-	// fmt.Println("sum3Number(3, 5, 7) =", sum3Number(3, 5, 7)) // 15
-	// fmt.Println("sum3Number(-10, 6, 4) =", sum3Number(-10, 6, 4)) // 0
-	// fmt.Println("=====")
+	fmt.Println("sum3Number(1, 2, 3) =", sum3Number(1, 2, 3)) // 6
+	fmt.Println("sum3Number(3, 5, 7) =", sum3Number(3, 5, 7)) // 15
+	fmt.Println("sum3Number(-10, 6, 4) =", sum3Number(-10, 6, 4)) // 0
+	fmt.Println("=====")
 
-	// fmt.Println("mean3Number(1, 2, 3) =", mean3Number(1, 2, 3))       // 2.0
-	// fmt.Println("mean3Number(4, 4, 5) =", mean3Number(4, 4, 5))       // 4.333
-	// fmt.Println("mean3Number(10, 20, 50) =", mean3Number(10, 20, 50)) // 26.667
-	// fmt.Println("=====")
+	fmt.Println("mean3Number(1, 2, 3) =", mean3Number(1, 2, 3))       // 2.0
+	fmt.Println("mean3Number(4, 4, 5) =", mean3Number(4, 4, 5))       // 4.333
+	fmt.Println("mean3Number(10, 20, 50) =", mean3Number(10, 20, 50)) // 26.667
+	fmt.Println("=====")
 
-	// fmt.Println("mean(1, 2, 3) =", mean([]int{1, 2, 3}))                                    // 2.0
-	// fmt.Println("mean(10, 20, 30, 40, 50) =", mean([]int{10, 20, 30, 40, 50}))              // 30.0
-	// fmt.Println("mean(1,2,3,4,5,6,7,8,9,10) =", mean([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})) // 4.5
-	// fmt.Println("=====")
+	fmt.Println("mean(1, 2, 3) =", mean([]int{1, 2, 3}))                                    // 2.0
+	fmt.Println("mean(10, 20, 30, 40, 50) =", mean([]int{10, 20, 30, 40, 50}))              // 30.0
+	fmt.Println("mean(1,2,3,4,5,6,7,8,9,10) =", mean([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})) // 4.5
+	fmt.Println("=====")
 
-	// fmt.Println(`isPalindrome("katak") =`, isPalindrome("katak"))
-	// fmt.Println(`isPalindrome("golang") =`, isPalindrome("golang"))
-	// fmt.Println(`isPalindrome("1234567890987654321") =`, isPalindrome("1234567890987654321")) // true
-	// fmt.Println(`isPalindrome("1234567890887654321") =`, isPalindrome("1234567890887654321")) // false
-	// fmt.Println("=====")
+	fmt.Println(`isPalindrome("katak") =`, isPalindrome("katak"))
+	fmt.Println(`isPalindrome("golang") =`, isPalindrome("golang"))
+	fmt.Println(`isPalindrome("1234567890987654321") =`, isPalindrome("1234567890987654321")) // true
+	fmt.Println(`isPalindrome("1234567890887654321") =`, isPalindrome("1234567890887654321")) // false
+	fmt.Println("=====")
 
-	// ans, found := findDuplicateNumber([]int{1, 2, 3, 3, 4, 5})
-	// fmt.Println("findDuplicateNumber(1,2,3,3,4,5) =", ans, found) // 3, true
-	// findDuplicateNumber([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 5})
-	// fmt.Println("findDuplicateNumber(1,2,3,4,5,6,7,8,9,5) =", ans, found) // 5, true
-	// fmt.Println("=====")
+	ans, found := findDuplicateNumber([]int{1, 2, 3, 3, 4, 5})
+	fmt.Println("findDuplicateNumber(1,2,3,3,4,5) =", ans, found) // 3, true
+	findDuplicateNumber([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 5})
+	fmt.Println("findDuplicateNumber(1,2,3,4,5,6,7,8,9,5) =", ans, found) // 5, true
+	fmt.Println("=====")
 
-	// fmt.Println("printTypeAndValue(12.34) =", printTypeAndValue(12))    // int: 12
-	// fmt.Println("printTypeAndValue(12.34) =", printTypeAndValue(12.34)) // float64: 12.34
-	// fmt.Println(`printTypeAndValue("tes") =`, printTypeAndValue("tes")) // string: tes
-	// fmt.Println("printTypeAndValue(true) =", printTypeAndValue(true))   // bool: true
-	// fmt.Println("=====")
+	fmt.Println("printTypeAndValue(12.34) =", printTypeAndValue(12))    // int: 12
+	fmt.Println("printTypeAndValue(12.34) =", printTypeAndValue(12.34)) // float64: 12.34
+	fmt.Println(`printTypeAndValue("tes") =`, printTypeAndValue("tes")) // string: tes
+	fmt.Println("printTypeAndValue(true) =", printTypeAndValue(true))   // bool: true
+	fmt.Println("=====")
 
-	// students := []model.Student{
-	// 	{Name: "Erwin", Score: 90},
-	// 	{Name: "a", Score: 75},
-	// 	{Name: "Jody", Score: 90},
-	// 	{Name: "b", Score: 70},
-	// 	{Name: "Irham", Score: 80},
-	// 	{Name: "c", Score: 65},
-	// 	{Name: "d", Score: 60},
-	// 	{Name: "Fatan", Score: 80},
-	// }
-	// fmt.Println("findStudents() =", findStudents(students, 80, false)) // 4 nama tidak terurut
-	// fmt.Println("findStudents() =", findStudents(students, 80, true))  // 4 nama terurut
-	// fmt.Println("findStudents() =", findStudents(students, 70, false)) // 6 nama tidak terurut
-	// fmt.Println("findStudents() =", findStudents(students, 70, true))  // 6 nama terurut
-	// fmt.Println("=====")
+	students := []model.Student{
+		{Name: "Erwin", Score: 90},
+		{Name: "a", Score: 75},
+		{Name: "Jody", Score: 90},
+		{Name: "b", Score: 70},
+		{Name: "Irham", Score: 80},
+		{Name: "c", Score: 65},
+		{Name: "d", Score: 60},
+		{Name: "Fatan", Score: 80},
+	}
+	fmt.Println("findStudents() =", findStudents(students, 80, false)) // 4 nama tidak terurut
+	fmt.Println("findStudents() =", findStudents(students, 80, true))  // 4 nama terurut
+	fmt.Println("findStudents() =", findStudents(students, 70, false)) // 6 nama tidak terurut
+	fmt.Println("findStudents() =", findStudents(students, 70, true))  // 6 nama terurut
+	fmt.Println("=====")
 
-	// st := students[0]
-	// changeName(&st, "Ethan")
-	// fmt.Println("st.Name =", st.Name)
+	st := students[0]
+	changeName(&st, "Ethan")
+	fmt.Println("st.Name =", st.Name)
 }
 
 func basics() {
